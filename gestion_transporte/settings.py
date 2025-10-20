91fea80d11f956bd5fc13517f90f291574ffaddf
Aaron montalvi <montalvo05.ac@gmail.com>
2025-10-20 15:37:54 -0600

    requeriments

--- gestion_transporte/settings.py
+++ gestion_transporte/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$c_s*b+$vvhn^df(bb$9np5tm189=9u*4gi%3vd9!jhxcr9_uy'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -165,7 +165,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'migmar.diesel@gmail.com'       # <-- TU CORREO
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Flota <estadisticas@transportesmigmar.com>'
 
